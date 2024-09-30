c1bf66f52cf80364556a0038e7c341c7c660117a
Emmanuel Iduh <emmiduh93@gmail.com>
2024-09-30 20:16:19 +0200

    Created UpdateTaskDTO.java

---
+++ src/main/java/com/fall2024devops/taskmanager/tasks/dto/UpdateTaskDTO.java
@@ -0,0 +1,32 @@
+package com.fall2024devops.taskmanager.tasks.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class UpdateTaskDTO {
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Input {
+        private String title;
+        private String description;
+        private String status;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Output {
+        private Long id;
+        private String title;
+        private String description;
+        private String status;
+        private Long userId;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+    }
+}
