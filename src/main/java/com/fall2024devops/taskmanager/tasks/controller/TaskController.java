cc662abb55d952e9a0aaf8577bf07103260f846c
Emmanuel Iduh <emmiduh93@gmail.com>
2024-10-02 00:21:40 +0200

    Updated TaskController.java

--- src/main/java/com/fall2024devops/taskmanager/tasks/controller/TaskController.java
+++ src/main/java/com/fall2024devops/taskmanager/tasks/controller/TaskController.java
@@ -1,14 +1,19 @@
 package com.fall2024devops.taskmanager.tasks.controller;
 
+import com.fall2024devops.taskmanager.tasks.dto.TaskDTO.TaskDTOOutput;
 import com.fall2024devops.taskmanager.common.response.GenericResponse;
 import com.fall2024devops.taskmanager.tasks.dto.CreateTaskDTO;
 import com.fall2024devops.taskmanager.tasks.dto.ListTasksDTO;
+import com.fall2024devops.taskmanager.tasks.dto.ListTasksDTO.Output;
 import com.fall2024devops.taskmanager.tasks.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -32,22 +37,22 @@ public class TaskController {
         ListTasksDTO.Output task = taskService.getTaskById(id);
         return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Task retrieved successfully", task));
     }
-//
-//    @GetMapping
-//    public ResponseEntity<GenericResponse<List<TaskDTO.TaskDTOOutput>>> getAllTasks() {
-//        List<TaskDTO.TaskDTOOutput> tasks = taskService.getAllTasks();
-//        return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Tasks retrieved successfully", tasks));
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public ResponseEntity<GenericResponse<TaskDTO.TaskDTOOutput>> updateTask(@PathVariable Long id, @RequestBody TaskDTO.TaskDTOInput taskDto) {
-//        TaskDTO.TaskDTOOutput updatedTask = taskService.updateTask(id, taskDto);
-//        return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Task updated successfully", updatedTask));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
-//        taskService.deleteTask(id);
-//        return ResponseEntity.noContent().build();
-//    }
+
+   @GetMapping
+   public ResponseEntity<GenericResponse<List<Output>>> getAllTasks() {
+       List<Output> tasks = taskService.getAllTasks();
+       return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Tasks retrieved successfully", tasks));
+   }
+
+   @PatchMapping("/{id}")
+   public ResponseEntity<GenericResponse<com.fall2024devops.taskmanager.tasks.dto.UpdateTaskDTO.Output>> updateTask(@PathVariable Long id, @RequestBody TaskDTOOutput taskDto) {
+       com.fall2024devops.taskmanager.tasks.dto.UpdateTaskDTO.Output updatedTask = taskService.updateTask(id, taskDto);
+       return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse<>("Task updated successfully", updatedTask));
+   }
+
+   @DeleteMapping("/{id}")
+   public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+       taskService.deleteTask(id);
+       return ResponseEntity.noContent().build();
+   }
 }
