bb680e3bfe3183a2625efaaae6554d93a54ee322
Emmanuel Iduh <emmiduh93@gmail.com>
2024-10-02 00:31:27 +0200

    Updates TaskDTO

--- src/main/java/com/fall2024devops/taskmanager/tasks/dto/TaskDTO.java
+++ src/main/java/com/fall2024devops/taskmanager/tasks/dto/TaskDTO.java
@@ -30,21 +30,16 @@ public class TaskDTO {
         private LocalDateTime updatedAt;
     }
 
-    public class TaskDTOOutput {
-
-        public String getTitle() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
-        }
-
-		public String getDescription() {
-			// TODO Auto-generated method stub
-			throw new UnsupportedOperationException("Unimplemented method 'getDescription'");
-		}
-
-		public String getStatus() {
-			// TODO Auto-generated method stub
-			throw new UnsupportedOperationException("Unimplemented method 'getStatus'");
-		}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TaskDTOOutput {
+        private Long id;
+        private String title;
+        private String description;
+        private String status;
+        private Long userId;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
     }
 }
